6a081567e4f93e4fc0f1352d937ec00616d5af45
songmingming <1659672814@qq.com>
2019-08-03 16:26:09 +0800

    集合其他小功能

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,6 @@
 <template>
   <div id="app">
+    {{title}}
     <img alt="Vue logo" src="./assets/logo.png">
     <HelloWorld msg="Welcome to Your Vue.js App"/>
      <el-input-number :key="list.key" :label="list.label" value="跑" controls-position="right" ></el-input-number>
@@ -24,12 +25,30 @@ export default {
   },
   data() {
     return {
+      title: '醉卧沙场君莫笑，古来征战几人回？',
       list: {
         key: 0,
         label: '40米大刀',
         value: '让你先跑'
       }
     }
+  },
+  created() {
+    this.TitleScrolling()
+  },
+  methods: {
+        TitleScrolling (){
+        setInterval(() => { //ES6箭头函数
+        	// 截取首字符串(第一个)
+            var head = this.title.substring(0,1)
+            // 截取除首字符串外所有字符串(除第一个所有)
+            var foot = this.title.substring(1)
+            // 头尾拼接后赋给data => tit属性
+            this.title = foot + head
+            // 最后赋给最终显示的标题(标题)
+            this.title = this.title
+        },600)
+     }
   }
 }
 </script>
